1feb89b0fd6ab78f639a9fa672bdb692e50266e9
Alex <careyalc@umich.edu>
2018-09-07 13:53:55 -0400

    pushing to github

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,7 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+now: Date;
+author: string;
+
   constructor(public navCtrl: NavController) {
+
+
+this.now = new Date();
+
+
+this.author = 'Alex Carey'; 
 
   }
 
